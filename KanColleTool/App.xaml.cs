bf275949350a9b0c84629e112ed575d64eb78d3c
Guan-Hwa, Chen <chen.guan.hwa@gmail.com>
2013-11-15 00:04:07 +0800

    艦娘

---
+++ KanColleTool/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace KanColleTool {
+    /// <summary>
+    /// App.xaml 的互動邏輯
+    /// </summary>
+    public partial class App : Application {
+    }
+}
